2e129979850f90ee91e066464cb6f312cca9d5c0
wcwalla <wcwalla@clemson.edu>
2021-05-19 17:55:40 -0400

    added res.json

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.get('/db', async (req, res) => {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM loads');
         const results = { 'results': (result) ? result.rows : null};
-        res.render('pages/db', results );
+        //res.render('pages/db', results );
+        res.json({ results: results });
         client.release();
     } catch (err) {
         console.error(err);
