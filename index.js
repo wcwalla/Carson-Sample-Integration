ae430ca136d811e8a684d4736ba5c65c6c8b9a34
wcwalla <wcwalla@clemson.edu>
2021-05-19 17:44:46 -0400

    Fixed get db

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/loads", (req, res) => {
 app.get('/db', async (req, res) => {
     try {
         const client = await pool.connect();
-        const result = await client.query('SELECT * FROM test_table');
+        const result = await client.query('SELECT * FROM loads');
         const results = { 'results': (result) ? result.rows : null};
         res.render('pages/db', results );
         client.release();
