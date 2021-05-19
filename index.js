83d0da8c2e33b9628bc67604e8882e97d4ba6da4
wcwalla <wcwalla@clemson.edu>
2021-05-19 14:19:49 -0400

    Added Routes

--- index.js
+++ index.js
@@ -2,12 +2,52 @@ const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000;
 const userData = require("./users.json");
+const loadData = require("./loads.json");
+var messageData = require("./messages.json");
+const fs = require("fs")
 
 app.get("/authenticate/:token", (req, res) => {
     var token = req.params.token
-    res.send(userData)
+    let user = userData.filter(function(item) { return item.api_token === token; })
+    res.send(user[0])
+})
+
+app.get("/loads", (req, res) => {
+    res.send(loadData)
+})
+
+app.put("/messages/:handle", (req, res) => {
+    var handle = req.params.handle
+    message = {handle: handle}
+    res.send(message)
+
+    fs.readFile('./messages.json', 'utf-8', function(err, data) {
+        if (err) throw err
+    
+        var messages = JSON.parse(data)
+        messages.push(message)
+
+    
+        fs.writeFile('./messages.json', JSON.stringify(messages), 'utf-8', function(err) {
+            if (err) throw err
+        })
+    })
 })
 
 app.listen(port, () => {
     console.log(`carsondemoservice is listening on port http://localhost:${port}`);
 })
+
+function jsonReader(filePath, cb) {
+    fs.readFile(filePath, (err, fileData) => {
+        if (err) {
+            return cb && cb(err)
+        }
+        try {
+            const object = JSON.parse(fileData)
+            return cb && cb(null, object)
+        } catch(err) {
+            return cb && cb(err)
+        }
+    })
+}
