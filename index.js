d7295bbd727798e470bdc513db4d88799abcb2be
wcwalla <wcwalla@clemson.edu>
2021-05-22 13:20:11 -0400

    All query functionality added for postgres

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000;
 
-const userData = require("./data/users.json");
-var messageData = require("./data/messages.json");
+require('dotenv').config()
 
-const fs = require("fs")
+const bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 // For parsing application/json
 app.use(express.json());
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const { Pool } = require('pg');
 const pool = new Pool({
-    connectionString: "postgres://hbahjhiabhbemk:59e20c1b6c555c5d5bdd01230abad56be708453f70c1942963d8b0d65bdf3e1f@ec2-54-87-112-29.compute-1.amazonaws.com:5432/d1bmt92emnfsss",
+    connectionString: process.env.DATABASE_URL,
     ssl: {
     rejectUnauthorized: false
     }
@@ -26,16 +26,12 @@ app.get("/authenticate/:token", async (req, res) => {
     
     token = req.params.token
 
-    // let user = userData.filter(function(item) { return item.api_token === token; })
-
-    // user.length != 0 ? res.status(200).json(user[0]) : res.status(401).send("Unauthorized.")
-
     try {
-        const client = await pool.connect();
-        const result = await client.query(
-            `SELECT * FROM users WHERE api_token = '${token}'`);
-        result.length != 0 ? res.status(200).json(result.rows[0]) : res.status(401).send("Unauthorized.")
-        client.release();
+
+        results = await executeQuery(
+            `SELECT * FROM users WHERE api_token = '${token}'`)
+
+        results.length != 0 ? res.status(200).json(results[0]) : res.status(401).send("Unauthorized.")
         
     } catch (err) {
         console.error(err);
@@ -46,59 +42,75 @@ app.get("/authenticate/:token", async (req, res) => {
 
 app.get('/loads', async (req, res) => {
 
-    if (!authenticateToken(req.headers["authorization"])) res.status(401).send("Unauthorized.")
+    if (invalidToken(req.headers["authorization"])) res.status(401).send("Unauthorized.")
     
     else {
+        
         try {
-            const client = await pool.connect();
-            const result = await client.query('SELECT * FROM loads');
-            const results = { 'results': (result) ? result.rows : null};
-            //res.render('pages/db', results );
-            res.json(results.results);
-            client.release();
+
+            results = await executeQuery('SELECT * FROM loads')
+
+            res.status(200).json(results);
+
         } catch (err) {
             console.error(err);
             res.status(400).send("Error " + err);
         }
+
+
     }   
 })
 
-app.put("/messages/:handle", (req, res) => {
+app.put("/messages/:handle", jsonParser, async (req, res) => {
+
     const handle = req.params.handle
     const body = req.body
 
-    if (!authenticateToken(req.headers["authorization"])) res.status(401).send("Unauthorized.")
+    if (await invalidToken(req.headers["authorization"])) res.status(401).send("Unauthorized.")
 
     else {
+
         try {
 
             if (!validateMsg(body)) throw err;
 
-            message = {
+            const msg = {
                 handle: handle,
                 direction: body.direction,
                 username: body.username, 
-                composed_at: body.composed_at,
-                read_at: body.read_at,
-                platform_received_at: body.platform_received_at,
                 message_type: body.message_type,
-                body: body ? body.body: null
+                composed_at: body.composed_at,
+                platform_received_at: body.platform_received_at,
+                body: body.body || null,
+                form_code: body.form_code || null,
+                form_date: body.form_date || '1000-01-01T00:00:00+00:00',
+                contact: body.contact || null,
+                read_at: body.read_at || '1000-01-01T00:00:00+00:00',
+                deleted_at: body.deleted_at || '1000-01-01T00:00:00+00:00',
+                in_reply_to_handle: body.in_reply_to_handle || null,
+                workflow_action: body.workflow_action || null
+                
             }
 
-
-            res.status(200).json( {handle: message.handle} )
-
-            fs.readFile('./data/messages.json', 'utf-8', function(err, data) {
-                if (err) throw err
+            console.log(msg)
             
-                var messages = JSON.parse(data)
-                messages.push(message)
+            await executeQuery(`INSERT INTO messages (handle, direction, username, message_type, composed_at, platform_received_at, body, form_code, form_date, contact, read_at, deleted_at, in_reply_to_handle, workflow_action) VALUES ('${msg.handle}', '${msg.direction}', '${msg.username}', '${msg.message_type}', '${msg.composed_at}', '${msg.platform_received_at}', '${msg.body}', '${msg.form_code}', '${msg.form_date}', '${msg.contact}', '${msg.read_at}', '${msg.deleted_at}', '${msg.in_reply_to_handle}', ${msg.workflow_action})`)
+
+
+            res.status(200).json( {handle: msg.handle} )
+
+            // fs.readFile('./data/messages.json', 'utf-8', function(err, data) {
+            //     if (err) throw err
+            
+            //     var messages = JSON.parse(data)
+            //     messages.push(message)
 
             
-                fs.writeFile('./data/messages.json', JSON.stringify(messages), 'utf-8', function(err) {
-                    if (err) throw err
-                })
-            })
+            //     fs.writeFile('./data/messages.json', JSON.stringify(messages), 'utf-8', function(err) {
+            //         if (err) throw err
+            //     })
+            // })
+
 
         } catch(err) {
             res.status(400).json( [{ description: "Invalid message data.", code: 400}])
@@ -108,7 +120,8 @@ app.put("/messages/:handle", (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`carsondemoservice is listening on port http://localhost:${port}`);
+    console.log(
+        `carsondemoservice is listening on port http://localhost:${port}`);
 })
 
 validateMsg = (msg) => {
@@ -117,7 +130,6 @@ validateMsg = (msg) => {
     if (!(msg.hasOwnProperty("direction") && 
         msg.hasOwnProperty("username") &&
         msg.hasOwnProperty("composed_at") &&
-        msg.hasOwnProperty("read_at") &&
         msg.hasOwnProperty("platform_received_at") &&
         msg.hasOwnProperty("message_type"))) {
             valid = false;
@@ -141,9 +153,30 @@ validateMsg = (msg) => {
 
 }
 
-authenticateToken = (token) => { 
+invalidToken = async (token) => { 
 
-    let user = userData.filter(function(item) { return item.api_token === token; })
-    
-    return user.length != 0
+    results = await executeQuery(
+        `SELECT * FROM users WHERE api_token = '${token}'`)
+
+    return results.length == 0
+
+}
+
+executeQuery = async (query) => {
+
+    try {
+
+        const client = await pool.connect();
+        const result = await client.query(query);
+
+        client.release();
+
+        return await result.rows
+        
+        
+    } catch (err) {
+        console.error(err);
+        return
+    }
+
 }
