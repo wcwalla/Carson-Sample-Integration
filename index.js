6043b3571416cb73d0b517690d7c713745b39bf8
wcwalla <wcwalla@clemson.edu>
2021-05-25 11:33:49 -0400

    added payroll

--- index.js
+++ index.js
@@ -190,6 +190,42 @@ app.get('/truck', async (req, res) => {
     }
 })
 
+app.get('/payroll', async (req, res) => {
+
+    if (req.headers["eleos-platform-key"] != process.env.ELEOS_PLATFORM_KEY) {
+        res.status(401).send("Unauthorized.")
+    }
+    else {
+
+        try {
+
+            payroll = await executeQuery('SELECT * FROM paychecks')
+
+            for (const i in payroll) {
+                
+
+                payroll[i]['details'] = []
+                details = await executeQuery(`SELECT * FROM paycheckdetails, details WHERE paycheckdetails.details_id = details.id AND paycheckdetails.paycheck_id = '${payroll[i].id}'`)
+                delete payroll[i].id
+                for (const j in details) {
+
+                    delete details[j].details_id
+                    delete details[j].paycheck_id
+                    delete details[j].id
+                    payroll[i].details.push(details[j])
+                }
+
+            
+            }
+            res.status(200).json({paychecks: payroll});
+
+        } catch (err) {
+            console.error(err);
+            res.status(400).send("Error " + err);
+        }
+    }
+})
+
 app.put("/messages/:handle", jsonParser, async (req, res) => {
 
     const handle = req.params.handle
